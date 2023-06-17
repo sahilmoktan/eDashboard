c192cbcac53112e001236623f1d7840d812477ca
sahilmoktan <moktanshail9@gmail.com>
2023-06-17 23:57:18 +0545

    router, make header, basic links

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './Nav';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<h1>Product Listining component</h1>}/>
+        <Route path="/add" element={<h1> Add Product component</h1>}/>
+        <Route path="/update" element={<h1>Update Product component</h1>}/>
+        <Route path="/logout" element={<h1>Logout component</h1>}/>
+        <Route path="/profile" element={<h1>Profile component</h1>}/>
+
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
