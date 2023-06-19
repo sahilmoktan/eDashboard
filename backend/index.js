9db993b95cf4522a5fcd415e5b24fadfca4e2be9
sahilmoktan <moktanshail9@gmail.com>
2023-06-19 09:08:31 +0545

    mongoose installed and done very basic connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require ("mongoose")
+
 
 const app = express();
+const connectDB = async ()=>{
+  mongoose.connect("mongodb://localhost:27017/e-comm")
+const productSchema = new mongoose.Schema({})
+const product = mongoose.model('product', productSchema)
+const data = await product.find()
+console.warn(data)
+}
+connectDB()
 
 app.get("/", (req, res) => {
   res.send("app is working hai...!");
