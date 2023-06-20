7f48b3f97f291cdf7b2efc6ea43fcf9e9ae11366
sahilmoktan <moktanshail9@gmail.com>
2023-06-20 23:54:32 +0545

    cors setup, api hit & data save, redirect page after register

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,21 @@
 const express = require("express");
-const mongoose = require ("mongoose")
-
+const cors = require("cors")
+const connectDb = require("./db/config");
+const Users = require("./model/Users");
 
 const app = express();
-const connectDB = async ()=>{
-  mongoose.connect("mongodb://localhost:27017/e-comm")
-const productSchema = new mongoose.Schema({})
-const product = mongoose.model('product', productSchema)
-const data = await product.find()
-console.warn(data)
-}
-connectDB()
 
-app.get("/", (req, res) => {
-  res.send("app is working hai...!");
+connectDb();
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/register", async (req, res) => {
+  res.send("register app is working hai...!");
+
+  const user =  await Users.create(req.body);
+  res.json({ message: "User Register successfully" });
+  
 });
 
 app.listen(5000);
--- front-end/src/Components/SignUp.js
+++ front-end/src/Components/SignUp.js
@@ -1,24 +1,65 @@
-import React,{useState} from 'react'
-
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function SignUp() {
-    const [name, setName]=useState('');
-    const [password, setPassword]=useState('');
-    const [email, setEmail]=useState('');
-    const collectData=()=>{
-      console.warn  (name, email, password)
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  
+  const navigate = useNavigate()
+  
+  const collectData = () => {
+    // console.warn(name, email, password);
+    
+    // const result = fetch("http://localhost:5000/register", {
+    //   method: "post",
+    //   body: JSON.stringify({ name, email, password }),
+    //   headers: {
+    //     "content-Type": "application/json",
+    //   },
+    // });
+    const result = {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({ name, email, password }),
+    };
+    fetch("http://localhost:5000/register", result);
+    if (result){
+      navigate('/')
     }
-  return (
 
-    <div className='register'>
-        
-    <h1>Register</h1>
-    <input className='inputBox' type='text' value={name} onChange={(e)=>setName(e.target.value)} placeholder='Enter Name'/>
-    <input className='inputBox' type='text' value={password} onChange={(e)=>setPassword(e.target.value)} placeholder='Enter Email'/>
-    <input className='inputBox' type='password' value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='Enter Password'/>
-    <button onClick={collectData} className="appbutton" type='button'> Sign Up</button>
+    console.log(result);
+  };
+  return (
+    <div className="register">
+      <h1>Register</h1>
+      <input
+        className="inputBox"
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Enter Name"
+      />
+      <input
+        className="inputBox"
+        type="text"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Enter Email"
+      />
+      <input
+        className="inputBox"
+        type="password"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Enter Password"
+      />
+      <button onClick={collectData} className="appbutton" type="button">
+        {" "}
+        Sign Up
+      </button>
     </div>
-  )
+  );
 }
 
-export default SignUp
+export default SignUp;
